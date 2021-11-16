f90c96eae4b46356f3bc0ea95105db55cfdae97f
Igor Buzykin <lexemz@gmail.com>
2021-11-16 21:57:54 +0300

    Initial Commit

---
+++ CustomLogo/CustomLogoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomLogoApp.swift
+//  CustomLogo
+//
+//  Created by Igor on 16.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomLogoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
