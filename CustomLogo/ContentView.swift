4e45e01403f678b0b82fb71beb7a5a7be7960648
Igor Buzykin <lexemz@gmail.com>
2021-11-17 03:47:42 +0300

    Create Animation

--- CustomLogo/ContentView.swift
+++ CustomLogo/ContentView.swift
@@ -8,15 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showIlluminate = false
+
     var body: some View {
-        IlluminateLogo()
-            .frame(width: 300, height: 300)
+        VStack {
+            Spacer()
+
+            if showIlluminate {
+                IlluminateLogo()
+                    .frame(width: 250, height: 250)
+                    .transition(.scale)
+            }
+
+            Spacer()
+            Button(action: buttonAction) {
+                Text(showIlluminate ? "HIDE ILLUMINATE" : "SHOW ILLUMINATE")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color(red: 71.0/255.0, green: 106.0/255.0, blue: 74.0/255.0))
+                    .clipShape(Capsule())
+            }
+            .padding(.bottom, 30)
+        }
+    }
+
+    private func buttonAction() {
+        withAnimation {
+            showIlluminate.toggle()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .frame(width: 300, height: 300)
     }
 }
